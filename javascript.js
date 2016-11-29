60fee82d889a33ee8f3430435e4c0d58bbc3a3b4
rogerchin85 <rogerchin85@gmail.com>
2016-11-28 23:13:54 -0800

    updated some javascript

--- javascript.js
+++ javascript.js
@@ -1,12 +1,3 @@
 $(document).ready(function(){
-	var topDiv = document.getElementById = 'top';
-    
-    $.ajax({
-        url:'top_content.html',
-        dataType:'html',
-        success:function(resp){
-            console.log(resp);
-            topDiv.innerhtml = resp;
-        }
-    });
+	$("#top").load("top_content.html");
 });
