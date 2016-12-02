7e4bc98d9cd0444efc18eff35263ff8bbae5bbce
rogerchin85 <rogerchin85@gmail.com>
2016-12-02 02:17:01 -0800

    uploaded partly completed demo.html

--- javascript.js
+++ javascript.js
@@ -5,3 +5,53 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#footer").load("footer.html");
 });
+
+$(function(){
+    $("#home_screen").hide();
+});
+
+$(function(){
+    $("#thumb").draggable();
+    $("#drop_area").droppable({
+        drop:function(event){
+            $("#thumb").fadeOut(600, function(){
+                $(this).remove();
+            });
+            
+            $("#drop_area").fadeOut(600, function(){
+                $(this).remove();
+            });
+            
+            $("#screen").fadeOut(600, function(){
+                $(this).remove();
+            });
+            
+            $("#home_screen").fadeIn(800, function(){
+                $(this).show();
+            });
+        }
+    });
+});
+
+/* $(function(){
+    $("#notification_bar").resizable({
+        animate:true,
+        maxHeight:585,
+        maxWidth:0,
+        minHeight:33,
+        minWidth:0,
+        grid:550,
+        handles:'n, s',
+        stop:function(event, ui){
+            $("#test").show();
+            
+            $("#notification").fadeOut(600, function(){
+                $(this).hide();
+            });
+            
+            $("#connections").fadeOut(600, function(){
+                $(this).hide();
+            });
+        }
+    });
+}); */
